13facaa44e2e39591539674247bd61bbeb3c041e
antonioaltamura <alfredopacino@alice.it>
2017-10-14 14:46:30 +0200

    design fix

--- server.js
+++ server.js
@@ -2,32 +2,34 @@ const fs = require('fs'),
 	http = require('http'),
 	port = 3000
 let serve = (fileName, contentType, res) => {
-	fs.readFile(fileName,'utf8', (err, data) => {
-            if (err) {
-                res.writeHead(404, {
-                    'Content-type': 'text/plain'
-                });
-                res.write('Page Was Not Found');
-                res.end();
-            }
-			res.writeHead(200, {
-                'Content-type': contentType
-            });
-            res.write(data);
-            res.end();
-        })
+	fs.readFile(fileName, 'utf8', (err, data) => {
+		if (err) {
+			res.writeHead(404, {
+				'Content-type': 'text/plain'
+			});
+			res.write('Page Was Not Found');
+			res.end();
+		}
+		res.writeHead(200, {
+			'Content-type': contentType
+		});
+		res.write(data);
+		res.end();
+	})
 }
-http.createServer( (req, res) => {
+http.createServer((req, res) => {
 	(req.url === '/sigma.min.js') ?
-		serve('sigma.min.js','text/javascript',res) :
+		serve('sigma.min.js', 'text/javascript', res) :
 	(req.url === '/jquery.min.js') ?
-		serve('jquery.min.js','text/javascript',res) :
-    (req.url === '/data.txt') ?
-		serve('data.txt','text/plain',res) :
-	serve('index.html','text/html',res);
+		serve('jquery.min.js', 'text/javascript', res) :
+	(req.url === '/style.css') ?
+		serve('style.css', 'text/css', res) :
+	(req.url === '/data.txt') ?
+		serve('data.txt', 'text/plain', res) :
+	serve('index.html', 'text/html', res);
 }).listen(port, (err) => {
-  if (err) {
-    return console.log('mala tempora currunt', err)
-  }
-  console.log(`server is listening on ${port}`)
+	if (err) {
+		return console.log('mala tempora currunt', err)
+	}
+	console.log(`server is listening on ${port}`)
 });
